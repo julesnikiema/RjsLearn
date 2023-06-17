bb88c79a987385acc218b65894244df7d4e88ea4
JNIKIEMA <bodi@iMac-de-BODI-2.local>
2023-06-17 12:39:17 +0000

    LA FIN DU projet d'apprentissage

--- gepa/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-    return(
-        <div className="home" > 
-        <h1> mY Progression  </h1>
-        <div>welcome to this site </div>
-        </div>
-    )
-}
